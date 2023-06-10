a0fb0d0cbb77bc81908b39c8f895ba1878bd42b1
Christian <chrisb2061@gmail.com>
2023-06-09 22:39:16 -0400

    create noonlight client

---
+++ internal/noonlight/noonlight_client.go
@@ -0,0 +1,145 @@
+package noonlight
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+
+
+func newNoonlightClient(apiKey, baseURL string) *NoonlightClient {
+	return &NoonlightClient{
+		APIKey:  apiKey,
+		BaseURL: baseURL,
+		HTTPClient: &http.Client{
+			Timeout: time.Second * 10,
+		},
+	}
+}
+
+// TriggerAlarm - makes a request to noonlight to trigger an alarm
+func (c *NoonlightClient) TriggerAlarm(requestBody *TriggerAlarmRequestBody) (*TriggerAlarmResponse, error) {
+	// Create the request body JSON
+	reqBody, err := json.Marshal(requestBody)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the HTTP request
+	req, err := http.NewRequest(http.MethodPost, c.BaseURL+"/alarms", bytes.NewBuffer(reqBody))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", c.APIKey)
+
+	// Send the request using the client's HTTPClient
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Read and process the response
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Unmarshal the response JSON
+	var alarmResponse TriggerAlarmResponse
+	err = json.Unmarshal(respBody, &alarmResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return &alarmResponse, nil
+}
+
+type CancelAlarmResponse struct {
+	Status    string    `json:"status"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+// CancelAlarm - updaets a triggered alar to be canceled.
+func (client *NoonlightClient) CancelAlarm(alarmID string) (*CancelAlarmResponse, error) {
+	url := fmt.Sprintf("%s/dispatch/v1/alarms/%s/status", client.BaseURL, alarmID)
+
+	requestBody := struct {
+		Status string `json:"status"`
+	}{
+		Status: "CANCELED",
+	}
+
+	requestBodyBytes, err := json.Marshal(requestBody)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(requestBodyBytes))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", client.APIKey)
+
+	resp, err := client.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to cancel alarm: %s", resp.Status)
+	}
+
+	cancelAlarmResponse := &CancelAlarmResponse{}
+	err = json.NewDecoder(resp.Body).Decode(cancelAlarmResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return cancelAlarmResponse, nil
+}
+
+type GetAlarmStatusResponse struct {
+	Status string `json:"status"`
+}
+
+// ...
+
+func (c *NoonlightClient) GetAlarmStatus(alarmID string) (*GetAlarmStatusResponse, error) {
+	// Construct the URL for the GET request
+	url := fmt.Sprintf("%s/dispatch/v1/alarms/%s/status", c.BaseURL, alarmID)
+
+	// Make the GET request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", "Bearer "+c.APIKey)
+
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Parse the response body
+	var statusResp struct {
+		Status string `json:"status"`
+	}
+	err = json.NewDecoder(resp.Body).Decode(&statusResp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &GetAlarmStatusResponse{
+		Status: statusResp.Status,
+	}, nil
+}
---
+++ internal/noonlight/noonlight_data.go
@@ -0,0 +1,51 @@
+package noonlight
+
+import (
+	"net/http"
+	"time"
+)
+
+// NoonlightAPI represents the client for interacting with the Noonlight API.
+type NoonlightAPI interface {
+	TriggerAlarm(address string) (*TriggerAlarmResponse, error)
+	CancelAlarm(alarmID string) (*CancelAlarmResponse, error)
+	GetAlarmStatus(alarmID string) (*GetAlarmStatusResponse, error)
+}
+
+type TriggerAlarmRequestBody struct {
+	Name     string       `json:"name"`
+	Phone    string       `json:"phone"`
+	Location LocationData `json:"location"`
+}
+
+type LocationData struct {
+	Coordinates *CoordinatesData `json:"coordinates,omitempty"`
+	Address     *AddressData     `json:"address,omitempty"`
+}
+
+type CoordinatesData struct {
+	Lat      float64 `json:"lat"`
+	Lng      float64 `json:"lng"`
+	Accuracy int     `json:"accuracy"`
+}
+
+type AddressData struct {
+	Line1 string `json:"line1"`
+	Line2 string `json:"line2"`
+	City  string `json:"city"`
+	State string `json:"state"`
+	Zip   string `json:"zip"`
+}
+
+type TriggerAlarmResponse struct {
+	ID        string    `json:"id"`
+	Status    string    `json:"status"`
+	CreatedAt time.Time `json:"created_at"`
+	OwnerID   string    `json:"owner_id"`
+}
+
+type NoonlightClient struct {
+	APIKey     string
+	BaseURL    string
+	HTTPClient *http.Client
+}

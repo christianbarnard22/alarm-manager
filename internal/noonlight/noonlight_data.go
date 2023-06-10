65c00dd3774b123c3eab53f3c0f5b27e033cd0bf
Christian <chrisb2061@gmail.com>
2023-06-10 13:19:55 -0400

    add required tags to noonlight

--- internal/noonlight/noonlight_data.go
+++ internal/noonlight/noonlight_data.go
@@ -8,34 +8,34 @@ import (
 
 // NoonlightAPI represents the client for interacting with the Noonlight API.
 type NoonlightAPI interface {
-	TriggerAlarm(ctx context.Context, address string) (*TriggerAlarmResponse, error)
+	TriggerAlarm(ctx context.Context, req TriggerAlarmResponse) (*TriggerAlarmResponse, error)
 	CancelAlarm(ctx context.Context, alarmID string) (*CancelAlarmResponse, error)
 	GetAlarmStatus(ctx context.Context, alarmID string) (*GetAlarmStatusResponse, error)
 }
 
 type TriggerAlarmRequestBody struct {
-	Name     string       `json:"name"`
-	Phone    string       `json:"phone"`
-	Location LocationData `json:"location"`
+	Name     string        `json:"name" validate:"required"`
+	Phone    string        `json:"phone" validate:"required"`
+	Location *LocationData `json:"location" validate:"required"`
 }
 
 type LocationData struct {
-	Coordinates *CoordinatesData `json:"coordinates,omitempty"`
-	Address     *AddressData     `json:"address,omitempty"`
+	Coordinates *CoordinatesData `json:"coordinates,omitempty" validate:"required"`
+	Address     *AddressData     `json:"address,omitempty" validate:"required"`
 }
 
 type CoordinatesData struct {
-	Lat      float64 `json:"lat"`
-	Lng      float64 `json:"lng"`
-	Accuracy int     `json:"accuracy"`
+	Lat      float64 `json:"lat" validate:"required"`
+	Lng      float64 `json:"lng" validate:"required"`
+	Accuracy int     `json:"accuracy" validate:"required"`
 }
 
 type AddressData struct {
-	Line1 string `json:"line1"`
+	Line1 string `json:"line1" validate:"required"`
 	Line2 string `json:"line2"`
-	City  string `json:"city"`
-	State string `json:"state"`
-	Zip   string `json:"zip"`
+	City  string `json:"city" validate:"required"`
+	State string `json:"state" validate:"required"`
+	Zip   string `json:"zip" validate:"required"`
 }
 
 type TriggerAlarmResponse struct {
